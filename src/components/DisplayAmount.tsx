8da7ae7c3b4f9d41a587f02850dc383e80194a50
dammy216 <dammy3612@icloud.com>
2024-11-04 08:38:45 +0900

    ファイル作成

---
+++ src/components/DisplayAmount.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DisplayAmount = () => {
+  return (
+    <div>
+      <div className="displayContainer">
+        <label htmlFor="">実包:</label>
+        <input type="number" />
+        <label htmlFor="">空砲</label>
+        
+      </div>
+    </div>
+  )
+}
+
+export default DisplayAmount

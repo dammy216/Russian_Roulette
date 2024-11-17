da735c5472f84c451ca8c289dd770a2d0eb8e232
dammy216 <dammy3612@icloud.com>
2024-11-17 10:22:14 +0900

    基本実施う

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,69 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import DisplayAmount from "./components/DisplayAmount";
 
 function App() {
+  const [liveAmount, setLiveAmount] = useState<number>(0);
+  const [emptyAmount, setEmptyAmount] = useState<number>(0);
+
+  //実弾を減らす
+  const handleDecrementLiveAmountClick = () => {
+    if (liveAmount > 0) {
+      setLiveAmount((value) => value - 1);
+    }
+  };
+
+  //空砲を減らす
+  const handleDecrementEmptyAmountClick = () => {
+    if (emptyAmount > 0) {
+      setEmptyAmount((value) => value - 1);
+    }
+  };
+  //実包と空砲を反転する
+  const handleReverseClick = () => {
+    setLiveAmount(emptyAmount);
+    setEmptyAmount(liveAmount);
+  };
+
+  //初期化ボタン
+  const handleResetClick = () => {
+    setLiveAmount(0);
+    setEmptyAmount(0);
+  };
 
   return (
-    <div>
-      
+    <div className="main">
+      <h1>BackShot Roulette</h1>
+      <div className="amountManager">
+        <DisplayAmount
+          liveAmount={liveAmount}
+          emptyAmount={emptyAmount}
+          setLiveAmount={setLiveAmount}
+          setEmptyAmount={setEmptyAmount}
+        />
+        <div className="amountOperationButtons">
+          <button
+            className="decrementLiveAmount"
+            onClick={handleDecrementLiveAmountClick}
+          >
+            実弾を減らす
+          </button>
+          <button
+            className="decrementEmptyAmount"
+            onClick={handleDecrementEmptyAmountClick}
+          >
+            空砲を減らす
+          </button>
+        </div>
+      </div>
+      <button className="reverseButton" onClick={handleReverseClick}>
+        弾反転
+      </button>
+      <button className="resetButton" onClick={handleResetClick}>
+        リセット
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/DisplayAmount.tsx
+++ src/components/DisplayAmount.tsx
@@ -1,16 +1,53 @@
-import React from 'react'
+import "../styles/displayAmount.css";
 
-const DisplayAmount = () => {
+type Props = {
+  liveAmount: number;
+  emptyAmount: number;
+  setLiveAmount: (amount: number) => void;
+  setEmptyAmount: (amount: number) => void;
+};
+
+const DisplayAmount = (props: Props) => {
   return (
     <div>
       <div className="displayContainer">
-        <label htmlFor="">実包:</label>
-        <input type="number" />
-        <label htmlFor="">空砲</label>
-        
+        <div className="liveBullets">
+          <label htmlFor="">実弾：</label>
+          <select
+            name="実弾"
+            value={props.liveAmount}
+            onChange={(e) => props.setLiveAmount(Number(e.target.value))}
+          >
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+          </select>
+        </div>
+        <div className="emptyBullets">
+          <label htmlFor="">空砲：</label>
+          <select
+            name="空砲"
+            value={props.emptyAmount}
+            onChange={(e) => props.setEmptyAmount(Number(e.target.value))}
+          >
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+          </select>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DisplayAmount
+export default DisplayAmount;
